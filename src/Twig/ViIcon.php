18b68bad94b4dd88c3e4265a4ca1d1eee4f473ab
ROBjkE <robert@fyrst.digital>
2026-05-15 12:20:54 +0700

    feat(theme): add cookie-banner component, add vi_icon twig function

---
+++ src/Twig/ViIcon.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fyrst\ViewsTheme\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Twig\Markup;
+
+class ViIcon extends AbstractExtension
+{
+    private string $bundlePath;
+
+    public function __construct()
+    {
+        $this->bundlePath = \dirname(__DIR__, 2);
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('vi_icon', [$this, 'iconMarkup']),
+        ];
+    }
+
+    public function iconMarkup(string $name, ?array $options = []): Markup
+    {
+        $pack = $options['pack'] ?? 'default';
+        $ariaHidden = $options['ariaHidden'] ?? true;
+        $ariaLabel = $options['ariaLabel'] ?? null;
+        $mode = $options['mode'] ?? 'svg';
+
+        if ($mode ===  'css') {
+            $classes = ['icon'];
+            if ($pack === 'default') {
+                $classes[] = "icon-{$name}";
+            } else if (is_string($pack)) {
+                $classes[] = "icon-{$name}-{$pack}";
+            }
+            $classAttr = \implode(' ', $classes);
+            $classAttr = "class=\"{$classAttr}\"";
+            $html = "<span {$classAttr}></span>";
+            return new Markup($html, 'UTF-8');
+        }
+
+        // Build SVG file path
+        $svgPath = $this->bundlePath . '/Resources/app/storefront/src/assets/icon/' . $pack . '/' . $name . '.svg';
+
+        $svg = '';
+        if (\file_exists($svgPath)) {
+            $svg = \file_get_contents($svgPath);
+        }
+
+        // Build CSS classes
+        $classes = ['icon', 'icon-' . $name];
+
+        $classString = \implode(' ', $classes);
+
+        $ariaHiddenAttr = $ariaHidden ? ' aria-hidden="true"' : '';
+        $ariaLabelAttr = $ariaLabel ? ' aria-label="' . \htmlspecialchars($ariaLabel, \ENT_QUOTES, 'UTF-8') . '"' : '';
+
+        $html = '<span class="' . $classString . '"' . $ariaHiddenAttr . $ariaLabelAttr . '>' . $svg . '</span>';
+
+        return new Markup($html, 'UTF-8');
+    }
+}
