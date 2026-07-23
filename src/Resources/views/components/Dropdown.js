a30eaef1a8ddb374a2c30140dd3e0d83d1d84caa
ROBJkE <robsenq@gmail.com>
2026-07-23 17:55:22 +0700

    add Account:Actions component

--- src/Resources/views/components/Dropdown.js
+++ src/Resources/views/components/Dropdown.js
@@ -24,46 +24,11 @@ export default class Dropdown extends ShopwareComponent {
         this._toggle?.setAttribute('aria-expanded', open ? 'true' : 'false')
 
         if (open) {
-            this._focusPanel()
             return
         }
 
         if (this._toggle && this._panel?.contains(document.activeElement)) {
             this._setFocus(this._toggle)
         }
-    }
-
-    _focusPanel() {
-        if (!this._panel) {
-            return
-        }
-
-        const focusables = this._getFocusableElements(this._panel)
-        this._setFocus(focusables[0] || this._panel)
-    }
-
-    _getFocusableElements(root) {
-        if (window.focusHandler?.getFocusableElements) {
-            return Array.from(window.focusHandler.getFocusableElements(root))
-        }
-
-        return Array.from(
-            root.querySelectorAll(
-                'a[href], button:not([disabled]), input:not([disabled]):not([type="hidden"]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"])',
-            ),
-        )
-    }
-
-    _setFocus(element) {
-        if (!element) {
-            return
-        }
-
-        if (window.focusHandler?.setFocus) {
-            window.focusHandler.setFocus(element, { focusVisible: true })
-            return
-        }
-
-        element.focus()
     }
 }
